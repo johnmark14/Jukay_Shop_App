644de4e823bf16090aede7f1f5d723bdac660e9c
johnmark14 <johmarkgabrentina@gmail.com>
2021-06-24 23:12:27 +0800

    Init commit but still have errors

---
+++ lib/category_menu_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import 'colors.dart';
+import 'model/product.dart' as prod;
+
+class CategoryMenuPage extends StatelessWidget {
+  final prod.Category currentCategory;
+  final ValueChanged<prod.Category> onCategoryTap;
+  final List<prod.Category> _categories = prod.Category.values;
+
+  const CategoryMenuPage({
+    Key? key,
+    required this.currentCategory,
+    required this.onCategoryTap,
+  });
+
+  Widget _buildCategory(prod.Category category, BuildContext context) {
+    final categoryString =
+        category.toString().replaceAll('Category.', '').toUpperCase();
+    final ThemeData theme = Theme.of(context);
+
+    return GestureDetector(
+      onTap: () => onCategoryTap(category),
+      child: category == currentCategory
+          ? Column(
+              children: [
+                SizedBox(height: 16.0),
+                Text(
+                  categoryString,
+                  style: theme.textTheme.bodyText1,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 14.0),
+                Container(
+                  width: 70.0,
+                  height: 2.0,
+                  color: kShrinePink400,
+                )
+              ],
+            )
+          : Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                categoryString,
+                style: theme.textTheme.bodyText1!.copyWith(
+                  color: kShrineBrown900.withAlpha(153),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(top: 40.0),
+        color: kShrinePink100,
+        child: ListView(
+          children: _categories
+              .map((prod.Category c) => _buildCategory(c, context))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
